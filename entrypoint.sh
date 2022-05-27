4b54906a9ff7d0e537736fd863e2a08604a6b197
dmitrydrynov <dmitry.drynov@gmail.com>
2022-05-27 16:46:34 +0300

    Update files for docker deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 
 composer install
 
-php artisan migrate:refresh --seed
+php artisan migrate --seed
 php artisan firefly-iii:upgrade-database
 php artisan passport:install --force
 
-line="0 3 * * * /usr/local/bin/php /var/www/artisan firefly-iii:cron"
+line="0 3 * * * /usr/local/bin/php /app/artisan firefly-iii:cron"
 (crontab -u $(whoami) -l; echo "$line" ) | crontab -u $(whoami) -
